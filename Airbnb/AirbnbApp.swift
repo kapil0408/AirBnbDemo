998976230e258cd012edf444ce15a871a70cbb0f
Kapil Goyal <kkgoyal@suyati.com>
2024-01-20 22:37:18 +0530

    Initial Commit

---
+++ Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by Kapil Kumar on 20/01/24.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
