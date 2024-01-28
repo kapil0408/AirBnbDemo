bb334c2c7b736f6755cc869f506ee0a731eb4059
Kapil Goyal <kkgoyal@suyati.com>
2024-01-28 13:32:20 +0530

    Add animation view

--- Airbnb/Core/Explore/DestinationSearchView.swift
+++ Airbnb/Core/Explore/DestinationSearchView.swift
@@ -7,10 +7,20 @@
 
 import SwiftUI
 
+enum DestinationSearchOptions{
+    
+    case location
+    case dates
+    case guests
+}
+
 struct DestinationSearchView: View {
     
     @Binding var show : Bool
     @State private var destination = ""
+    
+    @State private var selectedOption: DestinationSearchOptions = .location
+    
     var body: some View {
         VStack{
             Button {
@@ -25,36 +35,66 @@ struct DestinationSearchView: View {
             
             //Where to
             VStack(alignment: .leading){
-                Text("Where to?")
-                    .font(.title2)
-                    .fontWeight(.semibold)
-                
-                HStack{
-                    Image(systemName: "magnifyingglass")
-                        .imageScale(.small)
-                    TextField("Search destination", text: $destination)
-                        .font(.subheadline)
+                if selectedOption == .location {
+                    Text("Where to?")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                    
+                    HStack{
+                        Image(systemName: "magnifyingglass")
+                            .imageScale(.small)
+                        TextField("Search destination", text: $destination)
+                            .font(.subheadline)
+                    }
+                    .frame(height: 44)
+                    .padding(.horizontal)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 8.0)
+                            .stroke(lineWidth: 1.0)
+                            .foregroundStyle(Color(.systemGray4))
+                    }
                 }
-                .frame(height: 44)
-                .padding(.horizontal)
-                .overlay {
-                    RoundedRectangle(cornerRadius: 8.0)
-                        .stroke(lineWidth: 1.0)
-                        .foregroundStyle(Color(.systemGray4))
+                else
+                {
+                    CollapsedPickerView(title: "Where", description: "Add destination")
                 }
+
             }
             .padding()
             .background(.white)
             .clipShape(RoundedRectangle(cornerRadius: 12))
             .padding()
             .shadow(radius: 10)
+            .onTapGesture {
+                selectedOption = .location
+            }
             
             // date selection views
-            CollapsedPickerView(title: "When", description: "Add dates")
-
+            
+            VStack{
+                if selectedOption == .dates{
+                    Text("Show expanded view")
+                }
+                else{
+                    CollapsedPickerView(title: "When", description: "Add dates")
+                }
+            }
+            .onTapGesture {
+                selectedOption = .dates
+            }
             
             // num guest views
-            CollapsedPickerView(title: "Who", description: "Add guests")
+            VStack{
+                if selectedOption == .guests{
+                    Text("Show expanded view")
+                }
+                else{
+                    CollapsedPickerView(title: "Who", description: "Add guests")
+                }
+            }
+            .onTapGesture {
+                selectedOption = .guests
+            }
         }
     }
 }
