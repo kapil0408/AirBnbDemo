78b268001c0a58baf97c8b4aaa9a9cd18e7e6102
Kapil Goyal <kkgoyal@suyati.com>
2024-01-31 11:12:07 +0530

    Add mock data json and move file in its folder

---
+++ Airbnb/Core/Listings/Modal/Listing.swift
@@ -0,0 +1,150 @@
+//
+//  Listing.swift
+//  Airbnb
+//
+//  Created by Kapil Kumar on 30/01/24.
+//
+
+import Foundation
+
+struct Listing : Identifiable, Codable {
+    
+    let id: String
+    let ownerId : String
+    let ownerName: String
+    let ownerImageUrl: String
+    
+    let numberOfBedrooms: Int
+    let numberOfBaths: Int
+    let numberOfGuests: Int
+    let numberOfBeds: Int
+    
+    let latitude: Double
+    let longitude: Double
+    let imageURL: [String]
+
+    
+    let pricePerNight: Int
+    
+    let address: String
+    let city: String
+    let state: String
+    let title:String
+    var rating: Double
+    
+    var features:[ListingFeatures]
+    var amenities:[ListingAmenties]
+    let type: [ListingType]
+    
+}
+
+enum ListingFeatures: Int, Codable, Identifiable, Hashable {
+    case selfCheckIn
+    case superHost
+    
+    var id: Int {return self.rawValue}
+    
+    var imageName:String {
+        switch self{
+        case .selfCheckIn:
+            return "door.left.hand.open"
+        case .superHost:
+            return "modal"
+        }
+    }
+    
+    
+    var title: String {
+        switch self {
+        case .selfCheckIn:
+            return "Self check-in"
+        case .superHost:
+            return "Superhost"
+        }
+    }
+    
+    var subtitle: String {
+        switch self{
+        case .selfCheckIn:
+            return "Check yourself in with the keypad"
+        case .superHost:
+            return "Superhost are experienced, highly rated hosts who are commited to providing great starts for guest"
+        }
+    }
+}
+
+
+enum ListingAmenties: Int, Codable, Identifiable, Hashable{
+    
+    case pool
+    case kitchen
+    case wifi
+    case laundary
+    case tv
+    case alarmSystem
+    case office
+    case balcony
+    
+    var title: String{
+        switch self{
+        case .pool:
+            return "Pool"
+        case .kitchen:
+            return "Kitchen"
+        case .wifi:
+            return "Wifi"
+        case .laundary:
+            return "Laundary"
+        case .tv:
+            return "TV"
+        case .alarmSystem:
+            return "Alarm System"
+        case .office:
+            return "Office"
+        case .balcony:
+            return "Balcony"
+        }
+    }
+    
+    var imageName: String{
+        switch self{
+        case .pool:
+            return "figure.pool.swim"
+        case .kitchen:
+            return "fork.knife"
+        case .wifi:
+            return "wifi"
+        case .laundary:
+            return "washer"
+        case .tv:
+            return "tv"
+        case .alarmSystem:
+            return "checkerboard.shield"
+        case .office:
+            return "pencil.and.ruler.fill"
+        case .balcony:
+            return "building"
+        }
+    }
+    
+    var id: Int {return self.rawValue }
+}
+
+enum ListingType: Int, Codable, Identifiable, Hashable {
+    
+    case apartment
+    case house
+    case townHouse
+    case villa
+    
+    var description:String {
+        switch self {
+        case .apartment: return "Apartment"
+        case .house: return "House"
+        case .townHouse: return "Town Home"
+        case .villa: return "Villa"
+        }
+    }
+    
+    var id: Int { return self.rawValue }
+}
---
+++ Airbnb/Extension/DeveloperPreview.swift
@@ -0,0 +1,22 @@
+//
+//  DeveloperPreview.swift
+//  Airbnb
+//
+//  Created by Kapil Kumar on 30/01/24.
+//
+
+import Foundation
+
+class DeveloperPreview {
+    
+    var listings: [Listing] = [
+    
+        .init(id: NSUUID().uuidString, ownerId: NSUUID().uuidString, ownerName: "Kapil", ownerImageUrl: "mail-profile-photo", numberOfBedrooms: 4, numberOfBaths: 3, numberOfGuests: 4, numberOfBeds: 4, latitude: 25.6533, longitude: 26.9876, imageURL: ["listing1", "listing2","listing3","listing4"], pricePerNight: 9879, address: "UDB EcoScape", city: "Jaipur", state: "Rajasthan", title: "UDB Ecoscape", rating: 4.9, features: [.selfCheckIn, .superHost], amenities: [.wifi, .kitchen, .balcony, .laundary, .tv], type: [.villa, .house]),
+        
+        .init(id: NSUUID().uuidString, ownerId: NSUUID().uuidString, ownerName: "Kapil", ownerImageUrl: "mail-profile-photo", numberOfBedrooms: 4, numberOfBaths: 3, numberOfGuests: 4, numberOfBeds: 4, latitude: 25.6233, longitude: 26.9846, imageURL: ["listing1", "listing2","listing3","listing4"],  pricePerNight: 9879, address: "UDB EcoScape", city: "Jaipur", state: "Rajasthan", title: "UDB EcoHomes", rating: 4.9, features: [.selfCheckIn, .superHost], amenities: [.wifi, .kitchen, .balcony, .laundary, .tv], type: [.villa, .house]),
+        
+        .init(id: NSUUID().uuidString, ownerId: NSUUID().uuidString, ownerName: "Kapil", ownerImageUrl: "mail-profile-photo", numberOfBedrooms: 4, numberOfBaths: 3, numberOfGuests: 4, numberOfBeds: 4, latitude: 25.6531, longitude: 26.9872, imageURL: ["listing1", "listing2","listing3","listing4"], pricePerNight: 9879, address: "UDB EcoScape", city: "Jaipur", state: "Rajasthan", title: "Rudra Building", rating: 4.9, features: [.selfCheckIn, .superHost], amenities: [.wifi, .kitchen, .balcony, .laundary, .tv], type: [.villa, .house]),
+        
+        .init(id: NSUUID().uuidString, ownerId: NSUUID().uuidString, ownerName: "Kapil", ownerImageUrl: "mail-profile-photo", numberOfBedrooms: 4, numberOfBaths: 3, numberOfGuests: 4, numberOfBeds: 4, latitude: 25.6531, longitude: 26.9872,imageURL: ["listing1", "listing2","listing3","listing4"],  pricePerNight: 9879, address: "UDB EcoScape", city: "Jaipur", state: "Rajasthan", title: "Rudra Building", rating: 4.9, features: [.selfCheckIn, .superHost], amenities: [.wifi, .kitchen, .balcony, .laundary, .tv], type: [.villa, .house])
+    ]
+}
